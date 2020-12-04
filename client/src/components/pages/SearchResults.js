64b38791b0a4fa5b83d6eced5ede950336c0dcfe
Andy Garcia <acgarcia@ucdavis.edu>
2020-12-04 01:48:31 -0800

    updated header to test deployment

--- client/src/components/pages/SearchResults.js
+++ client/src/components/pages/SearchResults.js
@@ -5,19 +5,17 @@ import omdb from "../../apis/omdb.js";
 import noImg from "../../assets/images/noImg.jpg";
 
 class SearchResults extends Component {
+  // ${process.env.REACT_APP_OMDB_KEY}
   state = { searchResults: {} };
 
   // Fetches search results from the OMDB API
   fetchSearchResults = async (mount) => {
-    const searchResults = await omdb.get(
-      `?apikey=${process.env.REACT_APP_OMDB_KEY}`,
-      {
-        params: {
-          s: this.props.match.params.id,
-          type: "movie",
-        },
-      }
-    );
+    const searchResults = await omdb.get(`?apikey=bf495efe`, {
+      params: {
+        s: this.props.match.params.id,
+        type: "movie",
+      },
+    });
     if (mount) {
       this.setState({ searchResults: searchResults.data.Search });
     }
